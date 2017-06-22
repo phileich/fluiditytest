bba64ecd3af90076c0aab2891c7167567512dc06
Philipp Eichhammer <philipp.eichhammer@gmail.com>
2017-06-22 14:45:14 +0200

    Extended graph functionality

--- src/bftsmart/fluidity/graph/FluidityGraphNode.java
+++ src/bftsmart/fluidity/graph/FluidityGraphNode.java
@@ -10,6 +10,8 @@ public class FluidityGraphNode {
     private int dataCenterId;
     private ArrayList<Integer> replicas;
     private int maximumNumberOfReplicas;
+    private ArrayList<FluidityGraphEdge> inEdges;
+    private ArrayList<FluidityGraphEdge> outEdges;
 
     public FluidityGraphNode(int id, ArrayList<Integer> replicas, int maximumNumberOfReplicas) {
         this.dataCenterId = id;
@@ -17,7 +19,9 @@ public class FluidityGraphNode {
         this.maximumNumberOfReplicas = maximumNumberOfReplicas;
     }
 
-
+    public int getDataCenterId() {
+        return dataCenterId;
+    }
 
     public void addReplica(int replicaId) {
         this.replicas.add(replicaId);
@@ -33,5 +37,21 @@ public class FluidityGraphNode {
 
     public void setMaximumNumberOfReplicas(int maximumNumberOfReplicas) {
         this.maximumNumberOfReplicas = maximumNumberOfReplicas;
+    }
+
+    public void addInEdge(FluidityGraphEdge inEdge) {
+        this.inEdges.add(inEdge);
+    }
+
+    public void addOutEdge(FluidityGraphEdge outEdge) {
+        this.outEdges.add(outEdge);
+    }
+
+    public void deleteInEdge(FluidityGraphEdge inEdge) {
+        this.inEdges.remove(inEdge);
+    }
+
+    public void deleteOutEdge(FluidityGraphEdge outEdge) {
+        this.outEdges.remove(outEdge);
     }
 }
