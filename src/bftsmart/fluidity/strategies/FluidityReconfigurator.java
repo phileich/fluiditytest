27f8193f873ea4e77a116ba26de1ca7ca61a4de3
Philipp Eichhammer <philipp.eichhammer@msg-systems.com>
2017-07-10 11:05:55 +0200

    Added implementation comments to the fluidityreconfigurator

--- src/bftsmart/fluidity/strategies/FluidityReconfigurator.java
+++ src/bftsmart/fluidity/strategies/FluidityReconfigurator.java
@@ -4,4 +4,8 @@ package bftsmart.fluidity.strategies;
  * Created by philipp on 06.07.17.
  */
 public class FluidityReconfigurator {
+    private DistributionStrategy strategy;
+
+    // This class first completes the latency information of the graph with the one from the latency
+    // storage and then calls the strategy
 }
