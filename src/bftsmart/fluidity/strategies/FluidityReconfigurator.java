57e6a0499d931e09a8872b7728657a7ec56d65ed
Philipp Eichhammer <philipp.eichhammer@gmail.com>
2017-07-11 15:25:19 +0200

    Extended FluidityReconfigurator to reduce latencies

--- src/bftsmart/fluidity/strategies/FluidityReconfigurator.java
+++ src/bftsmart/fluidity/strategies/FluidityReconfigurator.java
@@ -3,11 +3,12 @@ package bftsmart.fluidity.strategies;
 import bftsmart.dynamicWeights.DynamicWeightController;
 import bftsmart.dynamicWeights.Latency;
 import bftsmart.dynamicWeights.LatencyStorage;
-import bftsmart.dynamicWeights.ServerLatency;
 import bftsmart.fluidity.FluidityController;
 import bftsmart.fluidity.graph.FluidityGraph;
 import bftsmart.reconfiguration.ServerViewController;
+import org.apache.commons.math3.stat.descriptive.rank.Median;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -44,6 +45,7 @@ public class FluidityReconfigurator implements Runnable {
 
     private FluidityGraph fillGraphWithLatency(List<Latency[]> serverLatencies) {
         FluidityGraph returnGraph = serverViewController.getCurrentView().getFluidityGraph();
+        List<FluidityGraphLatency> fluidityGraphLatencies = new ArrayList<>();
 
         // This class first completes the latency information of the graph with the one from the latency
         // storage and then calls the strategy
@@ -59,15 +61,105 @@ public class FluidityReconfigurator implements Runnable {
                 int nodeFrom = returnGraph.getNodeIdFromReplicaId(replicaFrom);
                 int nodeTo = returnGraph.getNodeIdFromReplicaId(replicaTo);
 
+                FluidityGraphLatency latencyEntry = new FluidityGraphLatency(nodeFrom, nodeTo, latencyValue);
+                fluidityGraphLatencies.add(latencyEntry);
+
                 // TODO Erst alle latenzen zwischen gleichen nodes speichern und dann
                 // erst auf einen Wert reduzieren und anschließend in Graph eintragen
-
-                if (nodeFrom != nodeTo) {
-                    returnGraph.changeEdgeLatencyData(nodeFrom, nodeTo, latencyValue);
-                }
             }
         }
 
+        return getGraphWithReducedLatencies(fluidityGraphLatencies);
+    }
+
+    private FluidityGraph getGraphWithReducedLatencies(List<FluidityGraphLatency> graphLatencies) {
+        FluidityGraph returnGraph = serverViewController.getCurrentView().getFluidityGraph();
+
+        for (FluidityGraphLatency fgL : graphLatencies) {
+            int from = fgL.getNodeIdFrom();
+            int to = fgL.getNodeIdTo();
+            FluidityGraphLatency tempObj = new FluidityGraphLatency(from, to);
+
+            ArrayList<Integer> indexList = indexOfAll(tempObj, graphLatencies);
+            ArrayList<FluidityGraphLatency> reduceList = new ArrayList<>();
+
+            for(int index : indexList) {
+                reduceList.add(graphLatencies.get(index));
+            }
+
+
+        }
+
         return returnGraph;
+    }
+
+    private ArrayList<Integer> indexOfAll(FluidityGraphLatency obj, List<FluidityGraphLatency> latencyList) {
+        ArrayList<Integer> tempList = new ArrayList<>();
+
+        for (int i = 0; i < latencyList.size(); i++) {
+            if (obj.equals(latencyList.get(i))) {
+                tempList.add(i);
+            }
+        }
+
+        return tempList;
+    }
+
+    private double medianReducer(ArrayList<Double> list) {
+        double[] value = new double[list.size()];
+        value = list.toArray(value); //TODO Fix it
+        Median median = new Median();
+
+        median.setData(value);
+
+        return median.evaluate();
+    }
+
+    private class FluidityGraphLatency {
+        private int nodeIdFrom;
+        private int nodeIdTo;
+        private double latencyValue;
+
+        public FluidityGraphLatency(int nodeIdFrom, int nodeIdTo) {
+            this.nodeIdFrom = nodeIdFrom;
+            this.nodeIdTo = nodeIdTo;
+            this.latencyValue = -1;
+        }
+
+        public FluidityGraphLatency(int from, int to, double value) {
+            this.nodeIdFrom = from;
+            this.nodeIdTo = to;
+            this.latencyValue = value;
+        }
+
+        public int getNodeIdFrom() {
+            return nodeIdFrom;
+        }
+
+        public int getNodeIdTo() {
+            return nodeIdTo;
+        }
+
+        public double getLatencyValue() {
+            return latencyValue;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            FluidityGraphLatency that = (FluidityGraphLatency) o;
+
+            if (nodeIdFrom != that.nodeIdFrom) return false;
+            return nodeIdTo == that.nodeIdTo;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = nodeIdFrom;
+            result = 31 * result + nodeIdTo;
+            return result;
+        }
     }
 }
