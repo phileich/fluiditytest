30343cf1c431d1ea5df310d681e571be44200509
Philipp Eichhammer <philipp.eichhammer@gmail.com>
2017-10-03 10:24:49 +0200

    Created new classes to implement the reconfiguration

---
+++ src/bftsmart/fluidity/cloudconnection/CloudConnector.java
@@ -0,0 +1,51 @@
+package bftsmart.fluidity.cloudconnection;
+
+import bftsmart.dynamicWeights.Latency;
+import bftsmart.dynamicWeights.MedianReducer;
+import bftsmart.fluidity.graph.FluidityGraph;
+import bftsmart.tom.util.Logger;
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CloudConnector implements Runnable {
+    private int id;
+    private FluidityGraph fluidityGraph;
+
+    public CloudConnector(int id, FluidityGraph fluidityGraph) {
+        this.id = id;
+        this.fluidityGraph = fluidityGraph;
+    }
+
+    @Override
+    public void run() {
+    InternalServiceProxy internalClient = new InternalServiceProxy(id + 100);
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream(4);
+            DataOutputStream dos = new DataOutputStream(out);
+
+
+            byte[] serializedFluidityGraph = SerializationUtils.serialize(fluidityGraph);
+            dos.writeInt(serializedFluidityGraph.length);
+            dos.write(serializedFluidityGraph);
+
+
+            byte[] reply = internalClient.invokeInternal(out.toByteArray());
+            if (reply != null) {
+                //Logger.println("Received Internal Consensus: " + new String(reply));
+                
+            } else {
+                Logger.println("Received Internal Consensus: NULL");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            internalClient.close();
+        }
+    }
+}
---
+++ src/bftsmart/fluidity/cloudconnection/InternalServiceProxy.java
@@ -0,0 +1,26 @@
+package bftsmart.fluidity.cloudconnection;
+
+import bftsmart.tom.ServiceProxy;
+import bftsmart.tom.core.messages.TOMMessageType;
+import bftsmart.tom.util.Extractor;
+
+import java.util.Comparator;
+
+public class InternalServiceProxy extends ServiceProxy {
+    public InternalServiceProxy(int processId) {
+        this(processId, null, null, null);
+    }
+
+    public InternalServiceProxy(int processId, String configHome) {
+        this(processId, configHome, null, null);
+    }
+
+    public InternalServiceProxy(int processId, String configHome, Comparator<byte[]> replyComparator,
+                                Extractor replyExtractor) {
+        super(processId);
+    }
+
+    public byte[] invokeInternal(byte[] request) {
+        return invoke(request, TOMMessageType.INTERNAL_CONSENSUS);
+    }
+}
